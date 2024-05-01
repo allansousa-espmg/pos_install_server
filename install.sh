12f047e15748aa7890c6598e685fc4a88058f52a
allansousa-espmg <allansousa.espmg@gmail.com>
2024-05-01 17:08:42 -0300

    Atualização do código principal e remoção de aplicações não mais necessárias

--- install.sh
+++ install.sh
@@ -1,13 +1,61 @@
 #!/bin/bash
 
-# Atualizar o sistema
-sudo apt update && sudo apt upgrade -y
+#####################################################################
+# Nome: Allan Sousa
+# Email: allan.sousa@esp.mg.gov.br
+#
+# Nome do programa: Pós-instalação Ubuntu Server 22.04LTS
+# Versão: 0.1
+# Descrição: Pós-instalação Ubuntu Server 22.04LTS
+#
+# CHANGELOG:
+# v0.1:
+#   - Adição de funções para modularização do código.
+#   - Verificação de erros de instalação e comandos.
+#   - Personalização da mensagem de boas-vindas.
+#####################################################################
 
-# Instalar pacotes
-sudo apt install zsh neovim neofetch micro bashtop exa curl wget tldr net-tools git curl wget cmatrix -y
+# Funções
 
-# Configurar Zsh como shell padrão (opcional)
-chsh -s $(which zsh)
+function atualizar_sistema() {
+  echo "Atualização do sistema"
+  echo "================================================================"
+  sudo apt update -y || exit 1
+  sudo apt upgrade -y || exit 1
+  sudo apt dist-upgrade -y || exit 1
+  sudo apt install ubuntu-restricted-extras -y || exit 1
+}
 
-# Mensagem de finalização
-echo "Atualização e instalação dos programas concluída!"
+function instalar_pacotes() {
+  echo "Instalação de pacotes"
+  echo "================================================================"
+  sudo apt install zsh neovim neofetch bashtop curl wget tldr net-tools git curl wget -y || exit 1
+}
+
+function finalizar_instalacao() {
+  echo "Finalização, atualização e limpeza"
+  echo "================================================================"
+  sudo apt update && sudo apt dist-upgrade -y || exit 1
+  sudo apt autoclean -y
+  sudo apt autoremove -y
+  echo "Fim da instalação"
+  echo "================================================================"
+  clear
+}
+
+atualizar_sistema
+
+instalar_pacotes
+
+# Instalar ZSH como shell padrão (opcional)
+
+echo -n "Deseja instalar o ZSH como shell padrão? (s/n) "
+read resposta
+
+if [[ $resposta =~ ^[Ss]$ ]]; then
+  sudo chsh -s /usr/bin/zsh $usuario
+fi
+
+finalizar_instalacao
+
+
